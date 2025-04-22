870f0d2489ec34a7fb04fa7678c1243ebf58e68c
Aarushyadav <akaaarushyadav786@gmail.com>
2025-04-22 14:17:29 +0530

    Addons

--- src/config/agenda.js
+++ src/config/agenda.js
@@ -11,6 +11,11 @@ const agenda = new Agenda({
     }
   },
   processEvery: '1 minute',
+  defaultConcurrency: 5,
+  maxConcurrency: 20,
+  lockLimit: 0,
+  defaultLockLimit: 0,
+  defaultLockLifetime: 10 * 60 * 1000,
   ensureIndex: true
 });
 
@@ -21,6 +26,18 @@ agenda.on('ready', async () => {
 
 agenda.on('error', (err) => {
   console.error('Agenda connection error:', err);
+});
+
+agenda.on('start', job => {
+  console.log(`Job ${job.attrs.name} starting at ${new Date()}`);
+});
+
+agenda.on('complete', job => {
+  console.log(`Job ${job.attrs.name} completed at ${new Date()}`);
+});
+
+agenda.on('fail', (err, job) => {
+  console.error(`Job ${job.attrs.name} failed with error: ${err.message}`);
 });
 
 module.exports = agenda;
